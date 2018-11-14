a55812c3eed62f98488e10eb2e4512cf712559db
vlzh <vlzh@pm.me>
2018-11-14 18:08:44 +0600

    random color, owner...

--- l4/index.js
+++ l4/index.js
@@ -1,15 +1,44 @@
 // Array         0         1        2        3
-var colors = ["yellow", "green", "black", "blue"];
+var COLORS = ["yellow", "green", "black", "blue"];
+var NAMES = ["Tom", "Pushok", "Luparik"];
+var OWNERS = ["Top", "Bill", "John"];
+
+function getRandomFromArray(array) {
+    var random_number = Math.random() * (array.length - 1);
+    var r_index = Math.round(random_number);
+    return array[r_index];
+}
 
 function getRandomColor() {
-    var random_number = Math.random() * (colors.length - 1);
-    var r_index = Math.round(random_number);
-    console.log(r_index);
-    return colors[r_index];
+    return getRandomFromArray(COLORS);
 }
 
-function createRandomCat() {}
-
-for (var i = 0; i < 100; i++) {
-    console.log(getRandomColor());
+function getRandomName() {
+    var random_name = getRandomFromArray(NAMES);
+    return random_name;
 }
+
+function getRandomOwner() {
+    return getRandomFromArray(OWNERS);
+}
+
+function createRandomCat() {
+    var _cat = {
+        name: getRandomName(),
+        old: 3,
+        color_eye: getRandomColor(),
+        owner: getRandomOwner()
+    };
+    return _cat;
+}
+
+var cat = createRandomCat();
+console.log(cat);
+
+//var cats = [];
+//var cat;
+//for (var i = 0; i < 4; i++) {
+//cat = createRandomCat();
+//cats.push(cat);
+//}
+//console.log(cats);
