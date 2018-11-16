3c3e53812d5e4a58d2a8f1182e643cdd1ab27812
vlzh <vlzh@pm.me>
2018-11-16 18:14:49 +0600

    find a cat by color

--- l4/index.js
+++ l4/index.js
@@ -4,8 +4,8 @@ var NAMES = ["Tom", "Pushok", "Luparik"];
 var OWNERS = ["Top", "Bill", "John"];
 
 function getRandomFromArray(array) {
-    var random_number = Math.random() * (array.length - 1);
-    var r_index = Math.round(random_number);
+    var random_number = Math.random() * array.length;
+    var r_index = Math.floor(random_number);
     return array[r_index];
 }
 
@@ -14,8 +14,7 @@ function getRandomColor() {
 }
 
 function getRandomName() {
-    var random_name = getRandomFromArray(NAMES);
-    return random_name;
+    return getRandomFromArray(NAMES);
 }
 
 function getRandomOwner() {
@@ -27,7 +26,8 @@ function createRandomCat() {
         name: getRandomName(),
         old: 3,
         color_eye: getRandomColor(),
-        owner: getRandomOwner()
+        owner: getRandomOwner(),
+        is_result: false
     };
     return _cat;
 }
@@ -44,6 +44,14 @@ function getByColor(_cats, color) {
     return result;
 }
 
+function getFirstByColor(_cats, color) {
+    for (var i = 0; i < _cats.length; i++) {
+        if (_cats[i].color_eye === color) {
+            return _cats[i];
+        }
+    }
+}
+
 var cats = [];
 for (var i = 0; i < 1000; i++) {
     var cat = createRandomCat();
@@ -51,4 +59,5 @@ for (var i = 0; i < 1000; i++) {
 }
 // cats = Array.length = 1000
 var green_eye_cats = getByColor(cats, "green");
-console.log(green_eye_cats);
+var green_eye_cat = getFirstByColor(cats, "gren");
+console.log(green_eye_cat);
