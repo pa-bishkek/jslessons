f4e07c4541c6910230371a75abd405f6e56d1f9c
vlzh <vlzh@pm.me>
2018-11-21 16:17:22 +0600

    leftNY

--- l6/index.js
+++ l6/index.js
@@ -1,11 +1,44 @@
-function getHeader() {
+var SECOND = 1000;
+var MINUTE = SECOND * 60;
+var HOUR = MINUTE * 60;
+var DAY = HOUR * 24;
+
+function get1Header() {
     var h = document.getElementById("header");
     return h;
 }
 
 function printTimer() {
     console.log("Hello!");
-    setTimeout(printTimer, 1000);
 }
 
-printTimer();
+// 15 days 20 hours 15 munutes 24 seconds
+//
+function leftNY() {
+    var now = new Date();
+    var ny = new Date(2019, 0);
+    var delta = ny.getTime() - now.getTime();
+
+    var days_count = Math.floor(delta / DAY);
+    delta -= days_count * DAY;
+    var hours_count = Math.floor(delta / HOUR);
+    delta -= hours_count * HOUR;
+    var minutes_count = Math.floor(delta / MINUTE);
+    delta -= minutes_count * MINUTE;
+    var seconds_count = Math.floor(delta / SECOND);
+
+    return (
+        days_count +
+        " days " +
+        hours_count +
+        " hours " +
+        minutes_count +
+        " minutes " +
+        seconds_count +
+        " seconds"
+    );
+}
+
+console.log(leftNY());
+
+// printTimer();
