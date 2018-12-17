fbb7c7887b4309d1eac71107b71f824f3f049cf3
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2018-12-18 02:52:25 +0600

    gulp

---
+++ custom_bootstrap/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
+
+gulp.task("sayHello", function() {
+    console.log("HELLO!");
+});
+
+gulp.task("sass", function() {
+    return gulp
+        .src("./sass/style.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", function() {
+    gulp.watch("./sass/**/*.scss", ["sass"]);
+});
